79894754f3019fd0eb67a99745dca8206be1b955
Samit Koyom <samitkoyom@gmail.com>
2026-07-16 12:24:57 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,6 @@
     width="200" 
     height="200">
     <h1>EDL-GEN API with Laravel and Flutter integration</h1>
-    <h2>ยินดีต้อนรับสู่ EDL-GEN</h2>
-    <h2>ຍິນດີຕ້ອນຮັບສູ່ EDL-GEN</h2>
+    <h2>ยินดีต้อนรับสู่ EDL-GEN V.1</h2>
+    <h2>ຍິນດີຕ້ອນຮັບສູ່ EDL-GEN V.1</h2>
 @endsection
